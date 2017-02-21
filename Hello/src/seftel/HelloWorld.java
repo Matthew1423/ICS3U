031002c648f7b6804838b62573a7b055e6e90dd6
Matthew Seftel <324902535@C-5Q3ZJS1.tdsb.on.ca>
2017-02-21 14:45:01 -0500

    Pushing my first program

---
+++ Hello/src/seftel/HelloWorld.java
@@ -0,0 +1,16 @@
+package seftel;
+/**
+ * HelloWorld.java	
+ * Prints Hello World
+ * 21/02/2017
+ * @author Matthew Seftel
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World ;)");
+
+	}
+
+}
